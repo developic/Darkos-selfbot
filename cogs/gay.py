e424530b701ca5ce6ac67354e498b00578d4d0eb
HC36d <harshitaschill@gmail.com>
2025-02-23 20:25:25 +0530

    selfbot

--- cogs/gay.py
+++ cogs/gay.py
@@ -1,5 +1,6 @@
 import random
 import urllib.parse
+import discord
 from discord.ext import commands
 
 class GayHeritage(commands.Cog):
@@ -7,12 +8,15 @@ class GayHeritage(commands.Cog):
         self.bot = bot
 
     @commands.command(name="gay", help="Check someone's gay heritage. Usage: !gay <user>")
-    async def gay(self, ctx, user: commands.MemberConverter):
+    async def gay(self, ctx, user: discord.Member = None):
+        if user is None:
+            user = ctx.author  # Default to command sender if no user is mentioned
+
         gay_percentage = random.randint(1, 100)
-        description = f"{user.username} is {gay_percentage}% gay!🌈"
+        description = f"{user.display_name} is {gay_percentage}% gay! 🌈"
         embed_url = f"https://benny.fun/api/embed?description={urllib.parse.quote(description)}&colour=52e6e5&big_image=false"
 
-        await ctx.send(f"[․]({embed_url})") 
+        await ctx.send(f"[․]({embed_url})")
 
 async def setup(bot):
     await bot.add_cog(GayHeritage(bot))
